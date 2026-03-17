5e6eca4983010fa5cf5b2e0416ba8a3c029adceb
mahendirans <88781391+mahendirans@users.noreply.github.com>
2026-03-17 17:18:52 +0530

    Add files via upload
    Week1

---
+++ Week1day1/assignment.js
@@ -0,0 +1,27 @@
+ //string
+ let userName="mahendiran"  
+ console.log(userName);
+ console.log(typeof userName);
+
+ //string
+
+ let organization ="test leaf"  
+ console.log(organization);
+ console.log(typeof organization);
+
+ //Number
+ let contactNumber =2222222222
+ console.log(contactNumber);
+ console.log(typeof contactNumber);
+
+ //Boolean
+ let knowsAutomation= false
+ console.log(knowsAutomation);
+ console.log(typeof knowsAutomation);
+
+ //undefined
+let usesPlaywright
+ console.log(usesPlaywright);
+ console.log(typeof usesPlaywright);
+ 
+
